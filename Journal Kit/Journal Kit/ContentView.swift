bffee26aa3cdc77599ef4ebf745541d604241f12
Vinter Marco <vintermarco98@gmail.com>
2024-01-24 18:31:12 +0200

    git testing 4

--- Journal Kit/Journal Kit/ContentView.swift
+++ Journal Kit/Journal Kit/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("NEW APP")
+            Text("New app")
         }
         .padding()
     }
