6d21db4169ec9f7756badab0bf700931b10a2ccf
Vinter Marco <vintermarco98@gmail.com>
2024-01-24 18:22:20 +0200

    project setup

---
+++ Journal Kit/Journal Kit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journal Kit
+//
+//  Created by Vinter Marco on 24.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Journal Kit/Journal Kit/Journal_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_KitApp.swift
+//  Journal Kit
+//
+//  Created by Vinter Marco on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Journal_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
